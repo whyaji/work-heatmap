b7fff85a6493be0290abd92f29272e4715ceb5a5
Wahyu Patriaji <wahyupatriaji@gmail.com>
2025-10-24 09:42:43 +0700

    chore: replace @vitejs/plugin-react with @vitejs/plugin-react-swc in package.json and bun.lock; update related imports in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import viteReact from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact()],
+  plugins: [tanstackRouter({ autoCodeSplitting: true }), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
